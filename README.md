f61775f33585c1225980960fe45a9b4e90727694
CHaBou <chabup@delean.fr>
2016-10-22 16:23:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# hyper-plugin-autoprofile
-Extension for Hyper.app to profile term according to shell prompt
+# hyper-autoprofile
+Extension for [Hyper.app](https://hyper.is) to configure terminal appearance according to current shell prompt
+
+## Install
+
+To install, edit `~/.hyper.js` and add `"hyper-autoprofile"` to `plugins`:
+
+```
+plugins: [
+  "hyper-autoprofile",
+], 
+```
+## Configuration
+Add `overlay` in your `~/.hyper.js` config.
+The configuration below shows the two differents sections : `prompts` and `profiles` :
+
+```js
+module.exports = {
+  config: {
+    // other configs...
+    autoProfile: {
+      prompts: [{
+          // 'MyHost:~ me$ ' default MacOS bash prompt
+          pattern: '^(\\S+):([/~].*) ([a-z_][a-z0-9_\\-\\.]*[\\$]?)[\\$#]\\s*$',
+          hostname: 1,
+          path: 2,
+          username: 3
+        },{
+          // 'me@MyHost:~$ ' default Linux bash prompt
+          pattern: '^([a-z_][a-z0-9_\\-\\.]*[\\$]?)@(\\S+):([\\/~].*)[\\$#]\\s*$',
+          username: 1,
+          hostname: 2,
+          path: 3
+        }
+      ],
+      profiles: [{
+          triggers: [
+            'root@',
+          ],
+          backgroundColor: '#400'
+        },
+        {
+          triggers: [
+            '@scotchbox'
+          ],
+          backgroundColor: '#040'
+        },
+        {
+          triggers: [
+            '/project'
+          ],
+          backgroundColor: '#004'
+        }
+      ],
+      stripAnsiColors: true //default
+    }
+  }
+  //...
+};
+```
+### autoProfile.prompts
+
+This section defines different pattern for parsing prompt components : username, host, path.
+
+For example, define a pattern for MacOS bash default prompt :
+```
+{
+  // 'MyHost:~ me$ '
+  pattern: '^(\\S+):([/~].*) ([a-z_][a-z0-9_\\-\\.]*[\\$]?)[\\$#]\\s*$',
+  hostname: 1,
+  path: 2,
+  username: 3
+}
+```
+
+### autoProfile.profiles
+
+This section is an ordered array of potential Profile. A Profile is composed by a list of `trigger` and style properties.
+`trigger` formats :
+* `'user@'` to specify `user`
+* `'@host'` to specify `host`
+* `'/path/to/directory'` or `'/directory'` to specify `path`
+* `'user@host'` to specify `user` and `host`
+* `'user@:/path'` to specify `user` and `path`
+* `'@host:/path'` to specify `host` and `path`
+* `'user@host:/path'` to specify `user` and `host` and `path`
+
+`user` and `host` components are strictly compared, `path` matches if it is included in prompt path. `'/tmp'` path will match `'/tmp'` and `'/path/to/tmp/subpath'`.
+
+All other properties of this section will be applied to Term if a trigger is matched. It could be any property of the main config section like `backgroundColor`, `cursorColor`, `fontSize`...
+
+### autoProfile.stripAnsiColors (Default: true)
+If enabled, ANSI escape sequences are stripped from input before trying to match triggers.
+See [here](http://ascii-table.com/ansi-escape-sequences.php) for more details.
+
+## Caveat
+
+Because of some tricky parsing, this plugin could not detect a shell change immediately. To force its detection, clearing terminal (`Ctrl+L`) could help.
+
+## Licence
+
+[MIT](LICENSE.md)
---
+++ index.js
@@ -0,0 +1,189 @@
+const getPromptInfos = function (prompts, data) {
+  let result;
+  prompts.forEach(prompt => {
+    if (result || !prompt.pattern || prompt.pattern.length === 0) {
+      return;
+    }
+    const pattern = new RegExp(prompt.pattern, 'gm');
+    const matches = pattern.exec(data);
+    if (matches && matches.length > 0) {
+      // Pattern has matched, now extract infos
+      const {username, hostname, path} = prompt;
+      result = {
+        username: username ? matches[username] : undefined,
+        hostname: hostname ? matches[hostname] : undefined,
+        path: path ? matches[path] : undefined
+      };
+    }
+  });
+  return result;
+};
+
+const getProfile = function (profiles, infos) {
+  let result;
+  profiles.forEach(profile => {
+    if (result || !profile.triggers || profile.triggers.length === 0) {
+      return;
+    }
+    let match = false;
+    profile.triggers.forEach(trigger => {
+      if (match) {
+        return;
+      }
+      const keys = Object.keys(trigger);
+      for (let i = 0; i < keys.length; i += 1) {
+        let key = keys[i];
+        if (key === 'path') {
+          if (infos[key].indexOf(trigger[key]) === -1) {
+            return;
+          }
+        } else if (trigger[key] !== infos[key]) {
+          return;
+        }
+      }
+      match = true;
+    });
+    if (match) {
+      result = profile;
+    }
+  });
+
+  return result;
+};
+
+const newline = {};
+
+exports.middleware = store => next => action => {
+  if (action.type === 'SESSION_PTY_DATA') {
+    let {data, uid} = action;
+    data = data.replace(/.*\033\[2J/mg,''); // Remove all char before ANSI sequence to erase line 'ESC[2J'
+    data = data.replace(/\033\]0.*?\007/mg,''); // Remove ANSI sequence trick to set xterm title
+
+    // Parse data only if it is a new line or if it contains a line beginning
+    if (!newline[uid] &&
+        data.indexOf('\n') === -1 &&
+        data.indexOf('\r') === -1 &&
+        data.indexOf('\033[?1034h') === -1 && // After a sudo
+        data.indexOf('\033[K') === -1 && // ANSI sequence to erase a line
+        data.indexOf('\033[2J') === -1) { // ANSI sequence to erase display
+      return next(action);
+    }
+
+    // If there is a '\n' or '\r' at the end of line, next data is a newline
+    const lastChar = data.charAt(data.length - 1);
+    newline[uid] = (lastChar === '\n' || lastChar === '\r');
+
+
+    const config = store.getState().ui.autoProfile ? store.getState().ui.autoProfile.config : {};
+    if (!config || !config.prompts || config.prompts.length === 0) {
+      console.warn('hyper-autoprofile enabled but no prompt config found');
+      return next(action);
+    }
+
+    if (config.stripAnsiColors !== false) {
+      data = data.replace(/\033\[[0-9;]+m/mg,'');
+    }
+
+    const promptInfos = getPromptInfos(config.prompts, data);
+    if (!promptInfos) {
+      return next(action);
+    }
+    const profile = getProfile(config.profiles, promptInfos);
+    store.dispatch({
+      type: 'AUTOPROFILE_SET',
+      uid,
+      profile: profile ? profile.style : undefined
+    });
+  }
+  next(action);
+};
+
+const parseTrigger = function (trigger) {
+  const result = {};
+  const idxAt = trigger.indexOf('@');
+  const idxColon = trigger.indexOf(':');
+
+  // username
+  if (idxAt > 0) {
+    result.username = trigger.substr(0, idxAt);
+  }
+  // hostname
+  if (idxAt >= 0 && idxAt < trigger.length - 1) {
+    result.hostname = trigger.substr(idxAt + 1, ((idxColon >= 0) ? idxColon : trigger.length) - idxAt - 1);
+  }
+  // path
+  if (idxAt === -1 || (idxColon >= 0 && idxColon < trigger.length - 1)) {
+    result.path = trigger.substr(idxColon + 1, trigger.length - idxColon - 1);
+  }
+
+  return result;
+};
+
+function formatConfiguration(autoProfileConfig) {
+  const formattedConfig = {};
+
+  if (!autoProfileConfig || !autoProfileConfig.prompts || !autoProfileConfig.profiles) {
+    return formattedConfig;
+  }
+  formattedConfig.prompts = autoProfileConfig.prompts;
+  const profiles = [];
+  autoProfileConfig.profiles.forEach(profile => {
+    const formattedProfile = {triggers:[], style:{}};
+    if (!profile.triggers || profile.triggers.length === 0) {
+      return;
+    }
+    profile.triggers.forEach(trigger => {
+      const criteria = parseTrigger(trigger);
+      if (!criteria || criteria.length === 0) {
+        return;
+      }
+      formattedProfile.triggers.push(criteria);
+
+    });
+    formattedProfile.style = Object.assign({},profile);
+    delete formattedProfile.style.triggers;
+    profiles.push(formattedProfile);
+  });
+  formattedConfig.profiles = profiles;
+  return formattedConfig;
+}
+
+exports.reduceUI = (state, action) => {
+  switch (action.type) {
+    case 'CONFIG_LOAD':
+    case 'CONFIG_RELOAD': {
+      const config = formatConfiguration(action.config.autoProfile);
+      return state.set('autoProfile', {config: config, sessions: {}});
+    }
+    case 'AUTOPROFILE_SET': {
+      return state.setIn(['autoProfile', 'sessions', action.uid], action.profile);
+    }
+    default:
+      return state;
+  }
+};
+
+exports.mapTermsState = (state, map) => {
+  if (!state.ui.autoProfile || !state.ui.autoProfile.sessions) {
+    return map;
+  }
+  return Object.assign({}, map, {profiles: state.ui.autoProfile.sessions});
+};
+
+exports.getTermGroupProps = (uid, parentProps, props) => {
+  return Object.assign({}, props, {profiles: parentProps.profiles});
+};
+
+exports.getTermProps = (uid, parentProps, props) => {
+  const {profiles} = parentProps;
+  if (!profiles) {
+    return props;
+  }
+  const profileProps = Object.assign({}, profiles[uid]);
+
+  if (profileProps.backgroundColor) {
+    // Weird trick because backgroundColor property is explicitly not forwarded to Term in Hyper.app
+    profileProps.customCSS = `body { background-color: ${profileProps.backgroundColor}; }`;
+  }
+  return Object.assign({}, props, profileProps);
+};
